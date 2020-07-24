1fb31d8abfedd36f00ba4cc0a23ad3099d475415
Adewumi Adeyemo <adexadey2@gmail.com>
2020-07-24 21:24:17 +0100

    Task 2 updated

--- app1.js
+++ app1.js
@@ -1,26 +1,5 @@
-/*var num1 = 3;
-var num2 = 5;
-let multiNum = 0;
-let multiNum2 = 0;
-
-for(var i=3; i<1000; i++) {
-    if ((i%num1)=== 3 || (i%num1)=== 3) {
-        multiNum++;
-    } 
-}
-
-for(var j=5; j<1000; j++) {
-    if ((j%num2)=== 0) {
-        multiNum2++;
-    } 
-}
-
-alert(`The sum of the multiples of ${num1} and ${num2} under 1000 is: ${multiNum + multiNum2}`);
-*/
-
-
-// Task 1
-// Finding sum of the multiples of 3 and 5 under 1000
+// Challenge 1
+// Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
 num1 = prompt("Please enter 3");
 num2 = prompt("Now enter 5");
 var sum = 0;
@@ -31,5 +10,50 @@ for(var i=1; i < 1000; i++) {
 }
 alert(`The sum of the multiples of ${num1} and ${num2} under 1000 is: ${sum}`);
 
-// Task 2
-// 
+// Challenge 2
+/* Write a JavaScript program which accepts a number as input and inserts dashes (-)
+between each two even numbers. For example if you accept 025468 the output should
+be 0-254-6-8 */
+ var setOfNumbers = prompt("Please enter a set of numbers");
+ var hyphenated = [setOfNumbers[0]];
+ for(var i = 1; i < setOfNumbers.length; i++) {
+	 if((setOfNumbers[i-1]%2 === 0) && setOfNumbers[i]%2 === 0) {
+		 hyphenated.push('-', setOfNumbers[i]);
+	 } else {
+		 hyphenated.push(setOfNumbers[i]);
+		}
+ }
+	 
+console.log(hyphenated.join('')); 
+ 
+// Challenge 3
+/*  */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
